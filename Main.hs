0b7a3e440b972cc447cebef88cb48eb768695406
Eric Rochester <erochest@virginia.edu>
2014-03-18 15:37:55 -0400

    Added command-line option parsing and version printing.

--- Main.hs
+++ Main.hs
@@ -1,8 +1,40 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 
 module Main where
 
 
+import qualified Data.Text           as T
+import           Data.Version
+import           Options.Applicative
+import           Paths_phaedrus
+
+
+data PhaedrusOpts = PhO
+                  { phoVersion :: Bool
+                  } deriving (Eq, Show)
+
+
+phaedrus :: PhaedrusOpts -> IO ()
+phaedrus PhO{ phoVersion = True } = do
+    putStrLn $ "phaedrus: version " ++ showVersion version
+
+
 main :: IO ()
-main = undefined
+main = execParser opts >>= phaedrus
+
+
+opts :: ParserInfo PhaedrusOpts
+opts = info (helper <*> phopts)
+            (  fullDesc
+            <> progDesc "Generate a training/test/dataset from the Dialogues."
+            <> header (  "phaedrus v" ++ showVersion version
+                      ++ ": dialogue dataset generator")
+            )
+
+phopts :: Parser PhaedrusOpts
+phopts =   PhO
+       <$> switch (  short 'V'
+                  <> long "version"
+                  <> help "Display the version information.")
