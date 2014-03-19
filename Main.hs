ea473918432872661e62f4e99d7ba3065f5f4d3f
Eric Rochester <erochest@virginia.edu>
2014-03-19 16:16:20 -0400

    Added data files.

--- Main.hs
+++ Main.hs
@@ -5,36 +5,55 @@
 module Main where
 
 
-import qualified Data.Text           as T
+import qualified Data.Text                 as T
 import           Data.Version
+import           Filesystem.Path.CurrentOS
+import qualified Filesystem.Path.CurrentOS as FS
 import           Options.Applicative
 import           Paths_phaedrus
+import           Prelude                   hiding (FilePath)
 
 
 data PhaedrusOpts = PhO
                   { phoVersion :: Bool
+                  , phoDataDir :: FilePath
                   } deriving (Eq, Show)
 
 
 phaedrus :: PhaedrusOpts -> IO ()
+
 phaedrus PhO{ phoVersion = True } =
     putStrLn $ "phaedrus: version " ++ showVersion version
 
+phaedrus phOpts = print phOpts
+
 
 main :: IO ()
-main = execParser opts >>= phaedrus
+main = do
+    dataDir <- decodeString <$> getDataDir
+    execParser (opts dataDir) >>= phaedrus
 
 
-opts :: ParserInfo PhaedrusOpts
-opts = info (helper <*> phopts)
-            (  fullDesc
-            <> progDesc "Generate a training/test/dataset from the Dialogues."
-            <> header (  "phaedrus v" ++ showVersion version
-                      ++ ": dialogue dataset generator")
-            )
+opts :: FilePath -> ParserInfo PhaedrusOpts
+opts dataDir =
+    info (helper <*> phopts dataDir)
+         (  fullDesc
+         <> progDesc "Generate a training/test/dataset from the Dialogues."
+         <> header (  "phaedrus v" ++ showVersion version
+                   ++ ": dialogue dataset generator")
+         )
 
-phopts :: Parser PhaedrusOpts
-phopts =   PhO
-       <$> switch (  short 'V'
-                  <> long "version"
-                  <> help "Display the version information.")
+phopts :: FilePath -> Parser PhaedrusOpts
+phopts dataDir =
+        PhO
+    <$> switch (  short 'V'
+                <> long "version"
+                <> help "Display the version information.")
+    <*> fileOpt (  short 'd'
+                <> long "data-dir"
+                <> value dataDir
+                <> help (  "The directory for the data (default = "
+                        ++ encodeString dataDir ++ ")."))
+
+fileOpt :: Mod OptionFields FilePath -> Parser FilePath
+fileOpt fields = nullOption (reader (pure . decodeString) <> fields)
